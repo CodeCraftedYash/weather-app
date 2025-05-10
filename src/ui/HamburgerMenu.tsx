9e35c0fc6f83ab445f38e20fe56a32f8fe6db2d9
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-10 13:07:31 +0530

    feat(nav): add hamburger menu for mobile device

--- src/ui/HamburgerMenu.tsx
+++ src/ui/HamburgerMenu.tsx
@@ -7,12 +7,12 @@ import { NavLink } from "react-router-dom";
 function HamburgerMenu({navItems}:{navItems:NavItem[]}) {
     const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-        {!isOpen?<FaBars size={20} onClick={() => setIsOpen(!isOpen)}/>:
-        <ImCross size={20}/>}
-        {isOpen?<ul className='sm:hidden gap-1 flex flex-col absolute top-16 right-1 bg-neutral-100 p-1 box-border rounded-md'>
+    <div className="sm:hidden">
+        {!isOpen?<FaBars size={20} onClick={() => setIsOpen(!isOpen)} className="transition-all"/>:
+        <ImCross size={20} onClick={() => setIsOpen(!isOpen)}/>}
+        {isOpen?<ul className='sm:hidden gap-1 flex flex-col absolute top-16 right-1 bg-neutral-100 p-1 box-border rounded-md shadow-xl'>
             {navItems.map((item) => (
-                <li key={item.to} className='whitespace-nowrap flex items-center gap-2 bg-[#2b95ff] p-2 rounded-md hover:cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
+                <li key={item.to} className='whitespace-nowrap flex items-center gap-2 bg-[#2b95ff] p-2 rounded-md hover:cursor-pointer' >
                     <NavLink
                         to={item.to}
                         className={({ isActive }) =>
@@ -20,6 +20,7 @@ function HamburgerMenu({navItems}:{navItems:NavItem[]}) {
                                 ? 'text-orange-700 font-bold sm:text-[1.2rem]'
                                 : 'text-white hover:text-blue-500 sm:text-[1.2rem]'
                         }
+                        onClick={() => setIsOpen(!isOpen)}
                     >
                         {item.name}
                     </NavLink> 
