6a58afd649b46de24595b9f02e6e8e8042ac425e
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-11 19:47:58 +0530

    fix(ui): adjust the code to change the color of active nav icons

--- src/ui/HamburgerMenu.tsx
+++ src/ui/HamburgerMenu.tsx
@@ -8,23 +8,24 @@ function HamburgerMenu({navItems}:{navItems:NavItem[]}) {
     const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="sm:hidden">
-        {!isOpen?<FaBars size={20} onClick={() => setIsOpen(!isOpen)} className="transition-all"/>:
+        {!isOpen?<FaBars size={20} onClick={() => setIsOpen(!isOpen)}/>:
         <ImCross size={20} onClick={() => setIsOpen(!isOpen)}/>}
-        {isOpen?<ul className='sm:hidden gap-1 flex flex-col absolute top-16 right-1 bg-neutral-100 p-1 box-border rounded-md shadow-xl'>
+        {isOpen?<ul className='sm:hidden gap-1 flex flex-col absolute top-16 right-1 bg-neutral-100 p-1 box-border rounded-xl shadow-xl z-50 border-4 border-[#2b95ff]'>
             {navItems.map((item) => (
                 <li key={item.to} className='whitespace-nowrap flex items-center gap-2 bg-[#2b95ff] p-2 rounded-md hover:cursor-pointer' >
                     <NavLink
                         to={item.to}
                         className={({ isActive }) =>
                             isActive
-                                ? 'text-orange-700 font-bold sm:text-[1.2rem]'
-                                : 'text-white hover:text-blue-500 sm:text-[1.2rem]'
+                                ? 'text-orange-300 font-bold sm:text-[1.2rem] flex items-center gap-2 custom-text-shadow'
+                                : 'text-white hover:text-blue-500 sm:text-[1.2rem] flex items-center gap-2'
                         }
                         onClick={() => setIsOpen(!isOpen)}
                     >
                         {item.name}
+                        <span>{item.icon}</span>
                     </NavLink> 
-                    <span className='text-white'>{item.icon}</span>
+                    
                 </li>
             ))}
         </ul>:""}
