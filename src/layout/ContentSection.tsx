e8479f3177cdab931e1525c5bba5b0f368f6e3d7
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-14 01:21:48 +0530

    fix(layout): fix the issue where the house image had oversize transparent background that came on top of search bar

--- src/layout/ContentSection.tsx
+++ src/layout/ContentSection.tsx
@@ -10,12 +10,11 @@ function ContentSection() {
 
         <div className="absolute box-border w-full bottom-0 h-12 lg:h-14 z-10 overflow-hidden "><div className="lg:translate-y-[-50px] lg:scale-y-[40%]  lg:scale-x-[130%] lg:translate-x-[110px] scale-x-[180%] translate-y-[-15px]"><Ground /></div></div>
 
-        <div className="absolute w-[30vw] aspect-square left-[60%] translate-x-[-50%] bottom-8 z-11 lg:bottom-[-130px]"><div className="scale-200 lg:scale-[70%]"><House /></div></div>
+        <div className="absolute w-32 lg:w-44 aspect-square left-[55%] -translate-x-[30%] bottom-5 lg:bottom-0 z-11"><House /></div>
 
         <div className="absolute w-[100px] aspect-square left-[20%] translate-x-[-50%] bottom-8 z-11 translate-y-3 lg:scale-[120%] lg:left-[30%] lg:bottom-[25px] lg:translate-x-[30%] "><WindMill/></div>
 
         <div className="absolute w-[20vw] aspect-square left-[50%] translate-x-[-50%] top-2 z-10"><div className="scale-[90%]"><SunMoon time="day" /></div></div>
-
         <div className="w-30 h-30 absolute overflow-hidden opacity-90"><div className="scale-[40%] absolute origin-top-left"><Cloud /></div></div>
     </div>
   )
