76f71397e8ce908218081242c5d60e4095d177d6
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-14 01:17:52 +0530

    feat(hooks): add useFetch hook to fetch data based on apis

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export function useFetch<T = any>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!url) return; 
+    const source = axios.CancelToken.source();
+
+    const fetchData = async () => {
+      setLoading(true);        
+      setError(null);          
+      setData(null);           
+
+      try {
+        const response = await axios.get<T>(url, {
+          cancelToken: source.token,
+        });
+        setData(response.data);
+      } catch (err: any) {
+        if (axios.isCancel(err)) {
+          console.log("Request canceled:", err.message);
+        } else {
+          setError(err.message || "Something went wrong");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      source.cancel("Request was canceled by the user.");
+    };
+  }, [url]);
+
+  return { data, loading, error };
+}
