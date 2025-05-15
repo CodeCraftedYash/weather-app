3db531c26f39286fb206ca6f8b441a054200f48a
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-15 23:27:18 +0530

    fix(home): improve  positioning of info section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
+import SearchBar from "../components/SearchBar"
 import ContentSection from "../layout/ContentSection"
 import InfoSection from "../layout/InfoSection"
-
 function Home() {
   return (
-    <main className="min-h-[calc(100dvh-10dvh)]">
+    <main className="min-h-[90dvh]">
         <ContentSection />
-        <InfoSection />
+        <div className="bg-gradient-to-b from-cyan-800 to-blue-200  md:flex-row w-[90%] md:w-[60%] min-h-[50dvh] border-4 rounded-2xl text-black mx-auto mt-2">
+           <div className="w-fit block md:hidden h-fit m-auto my-5"><SearchBar /></div>
+           <InfoSection />
+        </div>
     </main>
   )
 }
