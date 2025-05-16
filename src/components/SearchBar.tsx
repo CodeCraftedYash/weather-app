38894df2fddfb1746ea0b1f4d1701899795395f7
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-16 14:43:12 +0530

    fix(SearchBar): improve loading state by implementing loading component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,6 +3,7 @@ import { useFetch } from '../hooks/useFetch';
 import { useLocationStore } from '../store/locationStore';
 import { CiSearch } from "react-icons/ci";
 import { ImCross } from "react-icons/im";
+import Loading from '../ui/Loading';
 const SearchBar: React.FC = () => {
   const [query, setQuery] = useState('');
   const [url, setUrl] = useState('');
@@ -41,23 +42,23 @@ React.useEffect(() => {
 
 
   return (
-    <div className="md:w-[50%] flex items-center space-x-2 p-4 m-auto relative">
+    <div className="w-[100%] flex items-center  space-x-2 px-4 m-auto relative ">
       <input
        disabled={loading || flag}
         type="text"
         value={query}
         onChange={handleInputChange}
         placeholder="Search for a city..."
-        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="bg-white/90 w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
       <button
         disabled={loading || flag}
         onClick={() => handleSearch(query)}
-        className="px-4 py-2 text-white flex flex-nowrap items-center gap-1 bg-blue-500 rounded-md hover:bg-blue-600 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 hover:cursor-pointer"
+        className="px-4 py-2 text-white flex flex-nowrap items-center gap-1 border-2 border-white bg-blue-500 rounded-md hover:bg-blue-600 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 hover:cursor-pointer"
       >
         Search{!loading?<CiSearch className="" />:<ImCross className="text-red-500" />}
       </button>
-      {loading&&<div className='absolute left-[50%] translate-x-[-50%] top-[140%]'><div className='w-20 h-20 m-auto rounded-full border-t-4 animate-spin border-green-600 '></div></div>}
+      {loading&&<div className='absolute left-1/2 -translate-x-1/2 top-10'><Loading /></div>}
     {flag &&
         <div className="absolute left-[50%] translate-x-[-50%] top-2 text-red-500 bg-black p-4 rounded-xl text-center">
             <span className='absolute left-4 bottom-2 font-bold scale-200 animate-pulse'>!</span>
