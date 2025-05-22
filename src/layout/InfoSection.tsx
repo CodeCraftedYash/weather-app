c379dbad3e5e796186b975d66a1a0d728e963ae4
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-22 16:49:30 +0530

    refactor(InfoSection): remove needless line and adjust tailwind css code

--- src/layout/InfoSection.tsx
+++ src/layout/InfoSection.tsx
@@ -1,11 +1,17 @@
 import SearchBar from "../components/SearchBar"
+import { useLocationStore } from "../store/locationStore"
 import LocationDataLayout from "./LocationDataLayout"
-
+import WeatherDataLayout from "./WeatherDataLayout"
 function InfoSection() {
+  const {longitude} = useLocationStore();
   return (
-    <div className="w-[90%] md:w-[60%] min-h-[40dvh] border-4 rounded-2xl text-black mx-auto mt-2">
-      <SearchBar/>
-      <LocationDataLayout />  
+    <div className="w-[100%]  max-h-[16rem] md:max-h-[30rem] md:h-[23rem] flex flex-col gap-4 overflow-y-scroll md:flex-row">
+      <div className="flex flex-col items-center gap-2 ">
+        <div className="hidden md:block mt-4"><SearchBar/></div>
+        {longitude&&<LocationDataLayout />}
+      </div>
+        {longitude&&<WeatherDataLayout />}
+      
     </div>
   )
 }
