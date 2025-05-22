270c4d3f09cff0fb23277d67828428fbff8c557a
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-22 17:46:25 +0530

    style(home): adjust height of information section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <main className="min-h-[90dvh]">
         <ContentSection />
-        <div className="bg-gradient-to-b from-cyan-800 to-blue-200  md:flex-row w-[90%] md:w-[60%] min-h-[50dvh] border-4 rounded-2xl text-black mx-auto mt-2">
+        <div className="bg-gradient-to-b from-cyan-800 to-blue-200  md:flex-row w-[90%] md:w-[60%] min-h-[50dvh] max-h-[55dvh] border-4 rounded-2xl text-black mx-auto mt-2">
            <div className="w-fit block md:hidden h-fit m-auto my-5"><SearchBar /></div>
            <InfoSection />
         </div>
