3d4bb152c9fd962d02575d0a26e7804baacc81d3
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-24 19:05:28 +0530

    feat(Contact me): design basic Contact me page to direct to my portfolio

--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -1,6 +1,13 @@
+import ExternalSites from "../components/ExternalSites"
+
 function ContactMe() {
   return (
-    <div>ContactMe</div>
+    <div className="text-black min-h-[calc(100vh-20vh)] md:min-h-[calc(100vh-16vh)]">
+      <div className="w-[80%] md:w-[50%] mx-auto my-4 border-2 p-4 rounded-2xl">
+      <p>Hi, I'm Yash — a passionate Computer Science student diving deep into web development and full-stack technologies. I love building clean, responsive, and functional user interfaces using React, TypeScript, Tailwind CSS, and more.</p>
+      </div>
+      <ExternalSites />
+    </div>
   )
 }
 
