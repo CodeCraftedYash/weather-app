ada24dbf768c34e3b3cb66b690afa6906165ca20
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-24 18:57:13 +0530

    feat(footer): design  basic footer for the website

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <footer className="w-full bg-gray-900 text-gray-300 py-4 px-6 text-center text-sm">
+        <p>&copy; 2025 WeatherApp by Yash Mishra. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
