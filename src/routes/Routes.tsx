7d114c0890aa18a815e5b26830390947d1a1dd8c
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-24 19:06:38 +0530

    fix(routes): fix the third route to direct to contact me page

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
-import Weather from '../pages/ContactMe';
+import ContactMe from '../pages/ContactMe';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/weather" element={<Weather />} />
+      <Route path="/contact" element={<ContactMe />} />
     </Routes>
   );
 };
