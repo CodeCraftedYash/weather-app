21512f950ba388b0cfad02edb312d5ea4c7c7994
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-24 19:14:19 +0530

    feat(App): implement footer in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './layout/Footer';
 import Header from './layout/Header';
 import AppRoutes from './routes/Routes'
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <AppRoutes />
+      <Footer />
     </>
   )
 }
