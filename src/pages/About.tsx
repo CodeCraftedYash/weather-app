9df184669153fec929c86c951ee0742dcba432bc
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-05-24 19:04:40 +0530

    feat(About): design basic about section for the website introducin the features and tech behind

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,11 @@
 function About() {
   return (
-    <div>About</div>
+    <div className="text-lg min-h-[calc(100dvh-15dvh)] font-bold ">
+      <div className="custom-gradient h-full w-[90%] md:w-[60%] border-2 rounded-xl mx-auto mt-4 p-6">
+      <p>I developed this responsive weather application using React.js and Tailwind CSS, deploying it via Vercel for optimal performance. The app fetches real-time weather data from the Open-Meteo API, providing hourly forecasts for temperature, precipitation, and wind speed. For geolocation, it utilizes the Nominatim API from OpenStreetMap to convert user-inputted city names into geographic coordinates. The application leverages Axios for efficient API requests, Zustand for state management, and Chart.js (via react-chartjs-2) to visualize weather trends. Designed with a mobile-first approach, it ensures a seamless user experience across all screen sizes
+        </p>
+        </div>
+      </div>
   )
 }
 
